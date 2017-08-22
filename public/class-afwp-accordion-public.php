b02c165fc576f4cbe35c0afe5cdb1c22e3bb5a90
Theme Egg <themeeggofficial@gmail.com>
2017-08-22 08:20:16 +0545

    jQuery optimize

--- public/class-afwp-accordion-public.php
+++ public/class-afwp-accordion-public.php
@@ -1,107 +1,105 @@
 <?php
 
 /**
-* The public-facing functionality of the plugin.
-*
-* @link       http://themeegg.com/
-* @since      1.0.0
-*
-* @package    Accordion_For_WP
-* @subpackage Accordion_For_WP/public
-*/
+ * The public-facing functionality of the plugin.
+ *
+ * @link       http://themeegg.com/
+ * @since      1.0.0
+ *
+ * @package    Accordion_For_WP
+ * @subpackage Accordion_For_WP/public
+ */
 
 /**
-* The public-facing functionality of the plugin.
-*
-* Defines the plugin name, version, and two examples hooks for how to
-* enqueue the admin-specific stylesheet and JavaScript.
-*
-* @package    Accordion_For_WP
-* @subpackage Accordion_For_WP/public
-* @author     ThemeEgg <themeeggofficial@gmail.com>
-*/
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Accordion_For_WP
+ * @subpackage Accordion_For_WP/public
+ * @author     ThemeEgg <themeeggofficial@gmail.com>
+ */
 class Accordion_For_WP_Public {
 
-/**
-* The ID of this plugin.
-*
-* @since    1.0.0
-* @access   private
-* @var      string    $plugin_name    The ID of this plugin.
-*/
-private $plugin_name;
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $plugin_name The ID of this plugin.
+	 */
+	private $plugin_name;
 
-/**
-* The version of this plugin.
-*
-* @since    1.0.0
-* @access   private
-* @var      string    $version    The current version of this plugin.
-*/
-private $version;
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $version The current version of this plugin.
+	 */
+	private $version;
 
-/**
-* Initialize the class and set its properties.
-*
-* @since    1.0.0
-* @param      string    $plugin_name       The name of the plugin.
-* @param      string    $version    The version of this plugin.
-*/
-public function __construct( $plugin_name, $version ) {
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param      string $plugin_name The name of the plugin.
+	 * @param      string $version The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
 
-$this->plugin_name = $plugin_name;
-$this->version = $version;
+		$this->plugin_name = $plugin_name;
+		$this->version     = $version;
 
-}
+	}
 
-/**
-* Register the stylesheets for the public-facing side of the site.
-*
-* @since    1.0.0
-*/
-public function enqueue_styles() {
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
 
-/**
-* This function is provided for demonstration purposes only.
-*
-* An instance of this class should be passed to the run() function
-* defined in Accordion_For_WP_Loader as all of the hooks are defined
-* in that particular class.
-*
-* The Accordion_For_WP_Loader will then create the relationship
-* between the defined hooks and the functions defined in this
-* class.
-*/
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Accordion_For_WP_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Accordion_For_WP_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
 
-wp_enqueue_style( 'jquery-ui', plugin_dir_url( __FILE__ ) . 'css/jquery-ui-1.12.1.css', array(), $this->version, 'all' );
-wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/afwp-accordion-public.css', array(), $this->version, 'all' );
 
-}
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/afwp-accordion-public.css', array(), $this->version, 'all' );
 
-/**
-* Register the JavaScript for the public-facing side of the site.
-*
-* @since    1.0.0
-*/
-public function enqueue_scripts() {
+	}
 
-/**
-* This function is provided for demonstration purposes only.
-*
-* An instance of this class should be passed to the run() function
-* defined in Accordion_For_WP_Loader as all of the hooks are defined
-* in that particular class.
-*
-* The Accordion_For_WP_Loader will then create the relationship
-* between the defined hooks and the functions defined in this
-* class.
-*/
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
 
-wp_enqueue_script( 'jquery', plugin_dir_url( __FILE__ ) . 'js/jquery-1.12.4.js', array( ), $this->version, false );
-wp_enqueue_script( 'jquery-ui', plugin_dir_url( __FILE__ ) . 'js/jquery-ui-1.12.1.js', array( 'jquery' ), $this->version, false );
-wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/afwp-accordion-public.js', array( 'jquery-ui' ), $this->version, false );
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Accordion_For_WP_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Accordion_For_WP_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/afwp-accordion-public.js', array( 'jquery' ), $this->version, false );
 
-}
+	}
 
 }
 
